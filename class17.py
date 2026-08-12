1d367dc62ae7debd23d9594403208c4f752abeb9
Hasnainkhan20 <hasnaink5088@gmail.com>
2026-08-12 07:24:45 -0700

    Tuple

--- class17.py
+++ class17.py
@@ -1,3 +1,13 @@
-num = 1
-num2 = 6
-print(num + num2)
+# tuple
+a = (1, 2, 3,)
+b = ("ali", "ahmed", "basit",)
+c = (7.7, 5.6, 6.7, 9.0,)
+d = ("ahsan", 3, 4.6,)
+print(type(a))
+
+# list
+num1 = ["apple", "banana", "cheery" ]
+num2 = [1, 2, 3, 4]
+num3 = [4.2, 6.2, 5.3]
+num4 = ["mango", 3, 5.5]
+print(type(num1))
