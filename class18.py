483e5d9f91e66dedffb31819bd11fce814e19868
Hasnainkhan20 <hasnaink5088@gmail.com>
2026-08-13 07:40:43 -0700

    tuple  method

---
+++ class18.py
@@ -0,0 +1,43 @@
+# tuple
+# a = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10,)
+# print(a[6])
+
+
+# number = ()
+# for i in range (5):
+#     num = int(input("enter your num"))
+#     print(number)
+
+   
+#     # citynme  
+# cityname = ("karachi","lahore","islamabad","quetta","balochistan",)
+
+# # number
+# num = (10,20,30,40,50,)
+# print(num[-1])
+
+# alphabet
+# tuple = ("a","b","c","d","e",)
+# print(tuple[1:4])
+
+# number
+# b = (1,2,3,4,5,)
+# print(b[::-1])
+
+# . count
+# a = (5,3,5,5,2,5,)
+# print(a.count(5))
+
+# .index
+# b = (100,200,300,400,)
+# print(b.index(400))
+
+# pack tuple
+# value = ("Pakistan","karachi",)
+
+# unpack tuple
+num = ("john",30,"engineer")
+name,age,job = num 
+print(name)
+print(age)
+print(job)
