a4d243f61a05ea6e1d70b031e8e2dd196ddd3bd5
Hasnainkhan20 <hasnaink5088@gmail.com>
2026-08-20 07:42:15 -0700

    concatination

---
+++ class19.py
@@ -0,0 +1,27 @@
+# name = input("enter your name")
+# course = input("enter your course")
+# university_semester = int(input("enter your university_semester"))
+# city = input("enter your city")
+# print(f"my name is{name}I am learn{course}I studing in semester{university_semester}I am leaving in{city}")
+ 
+# use plus operator
+# date = input("enter your date")
+# month = input("enter your month")
+# year = input("enter your year")
+# print(date+month+year)
+
+# define temperature
+# temperature = input("enter your tempreature")
+# print(f"today's temperature is {temperature}")
+
+# lenght widht area
+
+# lenght = int(input("enter your lenght"))
+# widht = int(input("enter your widht"))
+# area = lenght * widht
+# print(f"Lenght: {lenght}, Width: {widht}, Area :{area}")
+
+word1 = input("enter your word1 ")
+word2 = input("enter your word2 ")
+variable = word1+word2
+print(variable)
