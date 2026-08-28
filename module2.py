c256cfe76b65cb51e2ce51bdf7c5d5306db9a0ae
Hasnainkhan20 <hasnaink5088@gmail.com>
2026-08-28 06:12:32 -0700

    module2

--- module2.py
+++ module2.py
@@ -42,9 +42,81 @@
 #     print("invalid input")
 
 # assignment operator
-a = 10
-a**= 2
+# a = 10
+# a**= 2
 
-g = 4
-g**=6 
-print(g)
+# g = 4
+# g**=6 
+# print(g)
+
+
+# CNIC
+# age = int(input("enter your age "))
+# cnic = input("Do you have cnic? {yes/no}")
+# if age >=18 and cnic == "yes":
+#     print("you can vote")
+# else:    
+#     print("you cannot vote")
+
+
+# debug 1
+# x = 10 
+# y = 3 
+# print(x / / y) 
+# x = 10 
+# y = 3 
+# print(x // y) 
+
+
+# debug2
+# a = 5 
+# # b = 10 
+# # if a = b:     
+# #     print("Equal") 
+# a = 5 
+# b = 10 
+# if a == b:    
+#   print("Equal") 
+
+# # debug3
+# # price = 500 
+# # discount = 10 
+# # final = price - price * discount / 100 
+# # prin(final)      
+# price = 500 
+# discount = 10 
+# final = price - price * discount / 100 
+# print(final) 
+
+
+#  debug4
+# x = 7 
+#  if x > 5 and < 10:     
+#   print("In range") 
+# x = 7 
+# if x > 5 and 10:     
+#    print("In range") 
+
+# debug5
+# items = ["pen", "book"] 
+# if "Pen" in items:     
+# print("Found")
+#  else:     
+# print("Not Found")
+# items = ["pen", "book"] 
+# if "Pen" in items:    
+#      print("Found") 
+# else:    
+#      print("Not Found")     
+
+# in operator 
+# fruit_list = ["banana", "grapes", "apple"]
+# item = input("enter your list ")
+# if item in fruit_list:
+#    print("true")
+# else:   
+#    print("false")
+
+# mini project
+unit = int(input("enter your unit "))
+print(unit * 25)
