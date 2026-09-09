22e43d45825a9a9347769b279e196353d9556d46
Hasnainkhan20 <hasnaink5088@gmail.com>
2026-09-09 07:15:01 -0700

    function

---
+++ class21.py
@@ -0,0 +1,37 @@
+# def countdown(n):
+#     if n == 0:
+#      return
+#     print(n)
+#     countdown(n -2)
+
+
+# countdown(20)
+
+
+
+# local global scope
+# def student_info():
+#    name = "ahsan"
+#    print(name)
+
+# student_info()
+
+
+
+# school = "MHK ACADEMY"
+# def show_school():
+#    print(school)
+
+# show_school()
+
+
+
+
+
+# def print_even(n):
+#       if n == 0: 
+#        return
+#       print(n)
+#       print_even(n - 2)
+
+# print_even(30)
---
+++ module 10.py
@@ -0,0 +1,61 @@
+# function module
+# coding exercise
+# q1
+# def average(num1, num2):
+#     return (num1 + num2) /2
+
+
+# print(average(40 , 35))
+
+
+
+# q2
+# def result(a=1 , b=2):
+#     print(a+b)
+
+# result()
+
+
+# q3
+# def maximum(*value):
+#     maxnumber = max(value)
+#     print(maxnumber)
+
+# maximum(50, 30, 90, 60, 10)
+
+
+# # q4
+# def student_info(**details):
+#     print(details)
+
+# student_info(name = "hasnain" , age = 17 , city = "karachi")
+
+
+
+# q5
+# cube = lambda a: a**3
+# print(cube(5))
+
+
+
+# q6
+# def sum_number(number):
+#     if number == 16:
+#      return
+#     numbers += sum_number
+#     print(sum_number)
+
+# sum_number(+1)
+
+
+
+
+# q7 
+counter = 10
+def update_counter ():
+   global counter
+   counter +=1
+update_counter()
+print(counter) 
+
+
